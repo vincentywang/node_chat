acda1532f79d0d4260a959503dfb58100545e8a6
vincentywang <vincentywang@gmail.com>
2016-03-25 06:32:46 +0800

    add user module

---
+++ model/user.js
@@ -0,0 +1,12 @@
+var passport = require('passport');
+var connection = require('../database.js')(passport);
+var chat = require('./chat.js');
+var util = require('../lib/util.js');
+
+
+var query = {};
+
+var user = {};
+
+
+module.exports = user;
